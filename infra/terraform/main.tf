93d7de4b02fc6ee83c0f5e632204ddf7d19bd711
Nik <kra-nik@yandex.ru>
2022-08-01 22:32:25 +1000

    add docker registry in yandex cloud

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -14,38 +14,3 @@ provider "yandex" {
   zone                     = var.zone
   service_account_key_file = var.service_account_key_file
 }
-
-resource "yandex_compute_instance" "docker" {
-  count                     = var.instance_count
-  name                      = "gitlab-host-${count.index}"
-  allow_stopping_for_update = true
-  labels = {
-    tags = "gitlab-host"
-  }
-  resources {
-    cores         = 2
-    memory        = 8
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = var.image_id
-      type     = "network-hdd"
-      size     = 50
-    }
-  }
-
-  network_interface {
-    # Указан id подсети default-ru-central1-a
-    subnet_id = var.subnet_id
-    nat       = true
-  }
-
-  scheduling_policy {
-    preemptible = true
-  }
-
-  metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-  }
-}
