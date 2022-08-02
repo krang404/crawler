cb138ec3ee3cd7100a5b1622b3cd73e46b5b4550
krang404 <77469890+krang404@users.noreply.github.com>
2022-08-02 23:35:02 +1000

    Docker image 1 (#3)
    * change terraform code
    
    * change playbook install docker
    
    * change pipeline
    
    * change playbook
    
    * change playbook
    
    * change playbook
    
    * add root in playbook
    
    * change pipeline
    
    * change pipeline
    
    * change pipeline
    
    * Add screencast
    
    * Add screencast
    
    * Add screencast

--- infra/terraform/registry.tf
+++ infra/terraform/registry.tf
@@ -3,11 +3,6 @@ data "yandex_iam_service_account" "srv_acc" {
   name = "srv-acc"
 }
 
-resource "yandex_container_registry" "registry" {
-  name      = "test-registry"
-  folder_id = var.folder_id
-}
-
 resource "yandex_container_registry_iam_binding" "puller" {
   registry_id = yandex_container_registry.registry.id
   role        = "container-registry.images.puller"
@@ -22,4 +17,9 @@ resource "yandex_container_registry_iam_binding" "pusher" {
   members = [
     "serviceAccount:${data.yandex_iam_service_account.srv_acc.id}",
   ]
+}
+
+resource "yandex_container_registry" "registry" {
+  name      = "test-registry"
+  folder_id = var.folder_id
 }
